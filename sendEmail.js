389b11b4f831f9cc07b1c226a52c1eb7efd5872e
Ferhat Avdic <ferhat.avdic@gmail.com>
2018-08-10 21:22:54 +0200

    email

--- sendEmail.js
+++ sendEmail.js
@@ -1,45 +1,67 @@
-var url = "https://simple-nodejs-email.herokuapp.com/api/sendemail";
+(function () {
+  
+  var url = "https://simple-nodejs-email.herokuapp.com/api/sendemail";
 // var url = "http://localhost:8080/api/sendemail";
 
-function sendEmail(nameVal, emailVal, messageVal){
-  var data = {
-    subject: nameVal + ' reached out to you',
-    email: emailVal,
-    text: messageVal
-  };
-  return fetch(url, {
-      method: 'POST', 
-    body: JSON.stringify(data),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-  }).then(res => res.json())
-    .then(response => {
-      if(response.statusCode)
-        if(response.statusCode !== 200) throw response.statusCode 
-        else $('.alert-success').show();
-      else {
-        $('.alert-success').show();
-      }
-    })
-    .catch(error => console.error('Error:', error));
-}
+  var sending = false;
 
-$('#emailBtn').click(function(event){
-  $('.alert-danger').hide();
-  event.preventDefault();
-  // console.log('clicked', $('#name').val(), $('#email').val(), $('#message').val());
-  if ($('#name').val() === "" || $('#email').val() === "" || $('#message').val() === ""){
-    $('.alert-danger').show();
-    return;
+  function sendEmail(nameVal, emailVal, messageVal){
+    sending = true;
+    var data = {
+      subject: nameVal + ' reached out to you',
+      email: emailVal,
+      text: messageVal
+    };
+    return fetch(url, {
+        method: 'POST', 
+      body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+    }).then(res => res.json())
+      .then(response => {
+        sending = false;
+        $('#emailBtn').empty();
+        $('#emailBtn').text('Send Email');
+        if(response.statusCode)
+          if(response.statusCode !== 200) throw response.statusCode 
+          else $('.alert-success').show();
+        else {
+          $('.alert-success').show();
+        }
+      })
+      .catch(error => {
+        sending = false;
+        $('.alert-danger').show();
+        $('.alert-danger').text('Unable to send mail at this time.');
+        // console.error('Error:', error);
+        $('#emailBtn').empty();
+        $('#emailBtn').text('Send Email');
+      });
   }
-  sendEmail($('#name').val(), $('#email').val(), $('#message').val());
-    
-});
 
-$('.close').click( function(event){
-  if($(event.target).has('.alert-danger'))
+  $('#emailBtn').click(function(event){
     $('.alert-danger').hide();
-  if ($(event.target).has('.alert-success'))
-    $('.alert-success').hide();
-});
+    $(event.target).empty();
+    $(event.target).append("<i class='fa fa-spinner fa-spin'></i>");
+    event.preventDefault();
+    // console.log('clicked', $('#name').val(), $('#email').val(), $('#message').val());
+    if ($('#name').val() === "" || $('#email').val() === "" || $('#message').val() === ""){
+      $('#emailBtn').empty();
+      $('#emailBtn').text('Send Email');
+      $('.alert-danger').show();
+      return;
+    }
+    if (!sending) sendEmail($('#name').val(), $('#email').val(), $('#message').val());
+  
+      
+  });
+
+  $('.close').click( function(event){
+    if($(event.target).has('.alert-danger'))
+      $('.alert-danger').hide();
+    if ($(event.target).has('.alert-success'))
+      $('.alert-success').hide();
+  });
+
+})();
